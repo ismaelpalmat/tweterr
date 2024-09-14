6742afc08380a6a545e5c469020b483b0c0e8d5d
Brayan Diaz C <brayandiazc@gmail.com>
2024-09-14 14:22:50 -0500

    se agrego faker

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id          :bigint           not null, primary key
+#  title       :string
+#  description :text
+#  author      :string
+#  available   :boolean          default(TRUE)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :author, presence: true
+  validates :available, inclusion: { in: [true, false] }
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id          :bigint           not null, primary key
+#  title       :string
+#  description :text
+#  author      :string
+#  available   :boolean          default(TRUE)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
