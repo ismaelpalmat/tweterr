f2bac80145b1618d1294fa1fec56990f4ecc05cf
Brayan Diaz C <brayandiazc@gmail.com>
2024-09-14 13:56:35 -0500

    cambios de contacto

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,8 @@
 class Contact < ApplicationRecord
+  validates :name,      presence: true,
+                        length: { minimum: 3 }
+  validates :email,     presence: true,
+                        format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :message,   presence: true,
+                        length: { maximum: 500 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :contacts
+  resources :contacts, only: %i[new create]
   get "pages/home"
   get "pages/terms"
   get "pages/faq"
